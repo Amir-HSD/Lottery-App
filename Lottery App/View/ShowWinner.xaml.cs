3f138d13dc295f3152e94084d3c431b598b30f04
AmirHSD <mosavian57@gmail.com>
2024-04-05 19:40:51 +0330

    Add ShowWinnerPage And MainPageViewModel

---
+++ Lottery App/View/ShowWinner.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Shapes;
+
+namespace Lottery_App.View
+{
+    /// <summary>
+    /// Interaction logic for ShowWinner.xaml
+    /// </summary>
+    public partial class ShowWinner : Window
+    {
+        public ShowWinner()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Storyboard storyboard = new Storyboard();
+
+            DoubleAnimation SpinnerAnimation = new DoubleAnimation();
+
+            SpinnerAnimation.From = 0;
+
+            SpinnerAnimation.To = 360;
+
+            SpinnerAnimation.Duration = new Duration(TimeSpan.FromSeconds(5));
+
+            SpinnerAnimation.RepeatBehavior = RepeatBehavior.Forever;
+
+            Storyboard.SetTarget(SpinnerAnimation, SpinnerTransform);
+
+            Storyboard.SetTargetProperty(SpinnerAnimation, new PropertyPath("Angle"));
+
+            storyboard.Children.Add(SpinnerAnimation);
+
+            storyboard.Begin();
+        }
+    }
+}
---
+++ Lottery App/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottery_App.ViewModel
+{
+    internal class MainPageViewModel : ViewBase
+    {
+
+    }
+}
