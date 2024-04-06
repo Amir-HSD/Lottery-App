0445cacecbf8a141854c518e3cfe06bfd231c9a3
AmirHSD <mosavian57@gmail.com>
2024-04-06 17:56:15 +0330

    Add SettingsPage

--- Lottery App/View/SettingsPage.xaml.cs
+++ Lottery App/View/SettingsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lottery_App.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,11 @@ namespace Lottery_App.View
     /// </summary>
     public partial class SettingsPage : Page
     {
+        SettingsPageViewModel SPVM = new SettingsPageViewModel();
         public SettingsPage()
         {
             InitializeComponent();
+            DataContext = SPVM;
         }
     }
 }
