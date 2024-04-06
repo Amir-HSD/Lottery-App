059397fecfc60afe9304544a397eebf27b4a0921
AmirHSD <mosavian57@gmail.com>
2024-04-06 18:09:16 +0330

    Complete the viewmodel

--- Lottery App/ViewModel/SettingsPageViewModel.cs
+++ Lottery App/ViewModel/SettingsPageViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Lottery_App.Base;
+using Lottery_App.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,59 @@ namespace Lottery_App.ViewModel
 {
     internal class SettingsPageViewModel : ViewBase
     {
+        public SettingsPageViewModel() {
+            ReservedNames = new ObservableCollection<Item>(UsersData.Instance.ReservedNames);
+        }
+
+        public ObservableCollection<Item> ReservedNames
+        {
+            get { return UsersData.Instance.ReservedNames; }
+            set
+            {
+                UsersData.Instance.ReservedNames = value;
+                OnPropertyChanged();
+            }
+        }
+        public Item selectedItem { get; set; }
+        public Item SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                selectedItem = value;
+                OnPropertyChanged(nameof(SelectedItem));
+            }
+        }
+
+        private string username = string.Empty;
+
+        public string UserName
+        {
+            get { return username; }
+            set { username = value; OnPropertyChanged(); }
+        }
+
+        public RelayCommand AddToListCommand => new RelayCommand(onExecute => { AddToList(); }, onExecuteChanged => { return true; });
+
+        public RelayCommand RemoveFromListCommand => new RelayCommand(onExecute => { RemoveFromList(); }, onExecuteChanged => { return true; });
+
+        public void AddToList()
+        {
+            if (UserName != string.Empty)
+            {
+                ReservedNames.Add(new Item { Name = UserName });
+                UserName = string.Empty;
+            }
+        }
+
+        public void RemoveFromList()
+        {
+            if (SelectedItem != null)
+            {
+                ReservedNames.Remove(SelectedItem);
+                SelectedItem = null;
+            }
+        }
+
     }
 }
